09e0c241baa4ca49443d2e77c939b285dbbd422b
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-03 02:24:52 +0100

    feat: add toast notifications composable with Sonner

---
+++ app/composables/useToast.ts
@@ -0,0 +1,26 @@
+import { toast } from 'vue-sonner'
+
+export const useToast = () => {
+  const success = (message: string) => {
+    toast.success(message)
+  }
+
+  const error = (message: string) => {
+    toast.error(message)
+  }
+
+  const info = (message: string) => {
+    toast.info(message)
+  }
+
+  const warning = (message: string) => {
+    toast.warning(message)
+  }
+
+  return {
+    success,
+    error,
+    info,
+    warning
+  }
+}

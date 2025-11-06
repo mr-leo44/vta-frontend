2bc412b16f84c2854d726327ebdbbaf3c4b78dc5
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-06 11:48:34 +0100

    feat: update auth middleware
    * update timeout and redirect URI

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,16 +1,22 @@
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   const authStore = useAuthStore()
 
   await authStore.$hydrate()
-  const isAuth = await authStore.checkAuth()
 
+  await nextTick()
+  const isAuth = await authStore.checkAuth()
   setTimeout(() => {
     if (!isAuth && to.path !== '/login') {
-      return navigateTo('/login')
+      // Sauvegarde la route demandée pour redirection après login
+      const redirectPath = to.fullPath || '/'
+      return navigateTo(`/login?redirect=${encodeURIComponent(redirectPath)}`)
     }
-  
+
+    // Si l'utilisateur est connecté et tente d'aller sur /login
     if (isAuth && to.path === '/login') {
-      return navigateTo('/')
+      // Redirige vers la page d'origine ou vers la racine si aucun "from"
+      const redirectPath = from.fullPath && from.fullPath !== '/login' ? from.fullPath : '/'
+      return navigateTo(redirectPath)
     }
-  }, 100)
+  }, 500)
 })
